2d75a58d4e61a278a9dc66c5267ffb13574a9943
rajatDevX <aneetajain205001@gmail.com>
2026-08-23 09:08:36 +0530

    Time: 0 ms (100%), Space: 9.5 MB (30.29%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,25 +1,34 @@
 class Solution {
-public:
-    vector<string>mapping={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+   vector<string> mapping = {
+        "", "", "abc", "def", "ghi",
+        "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
     vector<string>ans;
-    void backtrack(string &digits,int index,string &current){
+    void backtrack(string&digits,int index,string &current){
         if(index==digits.length()){
             ans.push_back(current);
-            return ;
+            return;
         }
-        char digit=digits[index];
-        string letters=mapping[digit-'0'];
-        for(char ch:letters){
-            current.push_back(ch);
+        char ch1=digits[index];
+
+        string words=mapping[ch1-'0'];
+
+
+        for(char word:words){
+            current.push_back(word);
+
             backtrack(digits,index+1,current);
+
             current.pop_back();
         }
-
     }
+public:
     vector<string> letterCombinations(string digits) {
-        if(digits.empty()){
-            return {};
-        }
+
+       if(digits.empty()){
+        return {};
+       }
         string current="";
         backtrack(digits,0,current);
         return ans;
