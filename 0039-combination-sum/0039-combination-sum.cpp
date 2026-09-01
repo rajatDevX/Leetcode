edf5944e7392018ab26153fb162cf1f32e5cab12
rajatDevX <aneetajain205001@gmail.com>
2026-09-01 22:29:18 +0530

    Time: 3 ms (51.73%), Space: 14.3 MB (46.07%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    vector<vector<int>>ans;
+    vector<int>current;
+void backtrack(vector<int>& candidates,int target,int start){
+    if(target==0){
+        ans.push_back(current);
+        return;
+    }
+    if(target<0){
+        return;
+    }
+    for(int i=start;i<candidates.size();i++){
+        current.push_back(candidates[i]);
+        backtrack(candidates,target-candidates[i],i);
+        current.pop_back();
+    }
+}
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        backtrack(candidates,target,0);
+        return ans;
+    }
+};
